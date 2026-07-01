a442de9bd225d51a5b8a139211a3dcf339208c4a
Spandana Eranaguru <ugs23148_it.spandana@cbit.org.in>
2026-07-01 23:28:38 +0530

    Time: 15 ms (70.71%), Space: 13.9 MB (58.35%) - LeetHub

---
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def minPathSum(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        m = len(grid)
+        n = len(grid[0])
+
+        # First row
+        for j in range(1, n):
+            grid[0][j] += grid[0][j-1]
+
+        # First column
+        for i in range(1, m):
+            grid[i][0] += grid[i-1][0]
+
+        # Remaining cells
+        for i in range(1, m):
+            for j in range(1, n):
+                grid[i][j] += min(grid[i-1][j], grid[i][j-1])
+
+        return grid[m-1][n-1]
