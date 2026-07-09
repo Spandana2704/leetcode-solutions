91cacf30dbfbd68747d93c270a9c43738ff29845
Spandana Eranaguru <ugs23148_it.spandana@cbit.org.in>
2026-07-09 23:03:01 +0530

    Time: 0 ms (100%), Space: 12.5 MB (17.17%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def summaryRanges(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[str]
+        """
+        res = []
+        n = len(nums)
+        i = 0
+
+        while i < n:
+            start = nums[i]
+
+            while i + 1 < n and nums[i + 1] == nums[i] + 1:
+                i += 1
+
+            end = nums[i]
+
+            if start == end:
+                res.append(str(start))
+            else:
+                res.append(str(start) + "->" + str(end))
+
+            i += 1
+
+        return res
