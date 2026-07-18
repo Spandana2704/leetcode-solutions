83c462f264cd4a9630e564f89e05d970ae0ac7c4
Spandana Eranaguru <ugs23148_it.spandana@cbit.org.in>
2026-07-18 22:47:11 +0530

    Time: 255 ms (75.57%), Space: 131.2 MB (75.52%) - LeetHub

---
+++ 0077-combinations/0077-combinations.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def combine(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        ans = []
+
+        def backtrack(start, path):
+            if len(path) == k:
+                ans.append(path[:])
+                return
+
+            for i in range(start, n + 1):
+                path.append(i)
+                backtrack(i + 1, path)
+                path.pop()
+
+        backtrack(1, [])
+        return ans
