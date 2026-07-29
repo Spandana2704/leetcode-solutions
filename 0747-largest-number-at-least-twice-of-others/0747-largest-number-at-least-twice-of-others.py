b9959ba03b69a50fc8d7a476dca87578d8c0b9aa
Spandana Eranaguru <ugs23148_it.spandana@cbit.org.in>
2026-07-30 00:28:43 +0530

    Time: 0 ms (100%), Space: 12.3 MB (90.61%) - LeetHub

---
+++ 0747-largest-number-at-least-twice-of-others/0747-largest-number-at-least-twice-of-others.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def dominantIndex(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        largest = -1
+        second = -1
+        index = -1
+
+        for i, num in enumerate(nums):
+            if num > largest:
+                second = largest
+                largest = num
+                index = i
+            elif num > second:
+                second = num
+
+        if largest >= 2 * second:
+            return index
+        return -1
