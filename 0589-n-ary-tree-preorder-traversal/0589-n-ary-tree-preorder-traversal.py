b314f84204877f6dec0fe6c229a2e728b6e65042
Spandana Eranaguru <ugs23148_it.spandana@cbit.org.in>
2026-08-13 21:46:49 +0530

    Time: 47 ms (7.1%), Space: 15.4 MB (88.71%) - LeetHub

---
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.py
@@ -0,0 +1,24 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution(object):
+    def preorder(self, root):
+        result = []
+
+        def dfs(node):
+            if not node:
+                return
+
+            result.append(node.val)
+
+            for child in node.children:
+                dfs(child)
+
+        dfs(root)
+        return result
+        
