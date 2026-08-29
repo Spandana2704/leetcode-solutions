4b61811e519de356320dfbf70046b7b30cfbcf51
Spandana Eranaguru <ugs23148_it.spandana@cbit.org.in>
2026-08-29 22:31:51 +0530

    Time: 651 ms (23.08%), Space: 37.8 MB (84.62%) - LeetHub

---
+++ 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def lexicographicallySmallestArray(self, nums, limit):
+        n = len(nums)
+
+        arr = sorted((nums[i], i) for i in range(n))
+
+        result = nums[:]
+        group = []
+
+        start = 0
+
+        while start < n:
+            end = start
+
+            while end + 1 < n and arr[end + 1][0] - arr[end][0] <= limit:
+                end += 1
+
+            values = [arr[i][0] for i in range(start, end + 1)]
+            indices = sorted(arr[i][1] for i in range(start, end + 1))
+
+            for i in range(len(indices)):
+                result[indices[i]] = values[i]
+
+            start = end + 1
+
+        return result
