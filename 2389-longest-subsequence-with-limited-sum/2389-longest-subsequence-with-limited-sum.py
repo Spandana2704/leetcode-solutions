7859376de96a9b37cca43c52ce93d96c0819ad87
Spandana Eranaguru <ugs23148_it.spandana@cbit.org.in>
2026-08-30 23:08:05 +0530

    Time: 106 ms (39.39%), Space: 12.4 MB (98.48%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.py
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.py
@@ -1,26 +1,18 @@
 class Solution(object):
     def answerQueries(self, nums, queries):
-
         nums.sort()
-
         prefix = []
         total = 0
-
         for num in nums:
             total += num
             prefix.append(total)
-
         answer = []
-
         for q in queries:
             count = 0
-
             for total in prefix:
                 if total <= q:
                     count += 1
                 else:
                     break
-
             answer.append(count)
-
         return answer
